0c5519cd2987b26145aabfac3b9a1377321e204e
hassanshabbir279 <87772719+hassanshabbir279@users.noreply.github.com>
2022-05-20 00:20:39 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import React from 'react'
+import TodoApp from './Components/TodoApp'
+import LoginForm from './Components/LoginForm'
+// import Spread from './Components/Spread'
+// import UseStatedeepExam from './Components/UseStatedeepExam'
+// import UseEffect from './Components/UseState'
+
+const App = () => {
+  return (
+    <div>
+      {/* <UseState/> */}
+      {/* <UseStatedeepExam/> */}
+      {/* <Spread/> */}
+      {/* <TodoApp/> */}
+       <LoginForm/>
+    </div>
+  )
+}
+
+export default App
